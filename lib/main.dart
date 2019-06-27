203487dacf9978c87f8b4f49d756b4c590cfdc16
Ashish Rawat <ashish@aeologic.com>
2019-06-27 18:51:00 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ChangeNotifierProvider<BottomNavigationBarProvider>(
+        child: BottomNavigationBarExample(),
+        builder: (BuildContext context) => BottomNavigationBarProvider(),
+      ),
+    );
+  }
+}
+
+class BottomNavigationBarExample extends StatefulWidget {
+  @override
+  _BottomNavigationBarExampleState createState() =>
+      _BottomNavigationBarExampleState();
+}
+
+class _BottomNavigationBarExampleState
+    extends State<BottomNavigationBarExample> {
+  var currentTab = [
+    Home(),
+    Profile(),
+    Setting(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    var provider = Provider.of<BottomNavigationBarProvider>(context);
+    return Scaffold(
+      body: currentTab[provider.currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: provider.currentIndex,
+        onTap: (index) {
+          provider.currentIndex = index;
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.person),
+            title: new Text('Profile'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Settings'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Container(
+        alignment: Alignment.center,
+        height: 300,
+        width: 300,
+        child: Text(
+          "Home",
+          style: TextStyle(color: Colors.white, fontSize: 30),
+        ),
+        color: Colors.amber,
+      )),
+    );
+  }
+}
+
+class Profile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          alignment: Alignment.center,
+          height: 300,
+          width: 300,
+          child: Text(
+            "Profile",
+            style: TextStyle(color: Colors.white, fontSize: 30),
+          ),
+          color: Colors.blue,
+        ),
+      ),
+    );
+  }
+}
+
+class Setting extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Container(
+        alignment: Alignment.center,
+        height: 300,
+        width: 300,
+        child: Text(
+          "Settings",
+          style: TextStyle(color: Colors.white, fontSize: 30),
+        ),
+        color: Colors.cyan,
+      )),
+    );
+  }
+}
+
+class BottomNavigationBarProvider with ChangeNotifier {
+  int _currentIndex = 0;
+
+  get currentIndex => _currentIndex;
+
+  set currentIndex(int index) {
+    _currentIndex = index;
+    notifyListeners();
+  }
+}
